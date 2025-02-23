1b645e7b80e065ae11892d1938a8c2870ce459f7
loukwn <main@loukwn.me>
2025-02-23 19:56:42 +0000

    Exercise 7

--- src/main/kotlin/coroutines/comment/CommentService.kt
+++ src/main/kotlin/coroutines/comment/CommentService.kt
@@ -15,10 +15,61 @@ class CommentService(
         collectionKey: String,
         body: AddComment
     ) {
-        TODO()
+        val userId = userService.readUserId(token)
+        val commentDocument = commentFactory.toCommentDocument(
+            userId = userId,
+            collectionKey = collectionKey,
+            body = body,
+        )
+        commentRepository.addComment(commentDocument)
     }
 
     suspend fun getComments(
         collectionKey: String
-    ): CommentsCollection = TODO()
+    ): CommentsCollection {
+        // (Naive: Calling findUserById on every comment)
+//        return coroutineScope {
+//            val commentElements = commentRepository.getComments(collectionKey)
+//                .map { async { userService.findUserById(it.userId) to it } }
+//                .awaitAll()
+//                .map { (user, comment) ->
+//                    CommentElement(
+//                        id = comment._id,
+//                        collectionKey = collectionKey,
+//                        user = user,
+//                        comment = comment.comment,
+//                        date = comment.date
+//                    )
+//                }
+//            CommentsCollection(
+//                collectionKey = collectionKey,
+//                elements = commentElements,
+//            )
+//        }
+        return coroutineScope {
+            val comments = commentRepository.getComments(collectionKey)
+            val uniqueUsers = comments
+                .map { it.userId }
+                .toSet()
+                .map { userId -> async { userId to userService.findUserById(userId) } }
+                .awaitAll()
+                .associate { it }
+
+            val commentElements = comments.map { comment ->
+                CommentElement(
+                    id = comment._id,
+                    collectionKey = collectionKey,
+                    user = uniqueUsers[comment.userId],
+                    comment = comment.comment,
+                    date = comment.date
+                )
+            }
+
+            CommentsCollection(
+                collectionKey = collectionKey,
+                elements = commentElements
+            )
+        }
+
+    }
 }
